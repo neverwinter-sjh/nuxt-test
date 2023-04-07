27ba6ea351c54181e245711a291789fd7dfb3401
neverwinter-sjh <neverwinter.sjh@gmail.com>
2023-04-07 05:26:32 +0000

    style 적용

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-standard-scss',
+    'stylelint-config-prettier-scss',
+    'stylelint-config-standard-vue',
+    'stylelint-config-prettier-scss',
+    'stylelint-config-standard-scss',
+    'stylelint-config-standard-vue/scss',
+    'stylelint-config-prettier-scss',
+  ],
   rules: {},
 }
