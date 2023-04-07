939d0d5584968c6ddbf2ac46672007f727d8ae86
neverwinter-sjh <neverwinter.sjh@gmail.com>
2023-04-07 05:37:47 +0000

    fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,5 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier',
-    'stylelint-config-standard-scss',
-    'stylelint-config-prettier-scss',
-    'stylelint-config-standard-vue',
-    'stylelint-config-prettier-scss',
-    'stylelint-config-standard-scss',
-    'stylelint-config-standard-vue/scss',
-    'stylelint-config-prettier-scss',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue'],
   overrides: [
     {
       files: ['**/*.vue'],
